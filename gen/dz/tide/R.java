1fb37cd35a25eed4443d43370018249230e58135
dzakarov@gmail.com <dzakarov@gmail.com>
2013-04-19 16:57:59 +0100

    init 5

--- gen/dz/tide/R.java
+++ gen/dz/tide/R.java
@@ -10,31 +10,52 @@ package dz.tide;
 public final class R {
     public static final class attr {
     }
+    public static final class bool {
+        public static final int must_log_time=0x7f060000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int firstHigh=0x7f050006;
-        public static final int firstHighDepth=0x7f050008;
-        public static final int firstHighTime=0x7f050007;
-        public static final int firstLow=0x7f050002;
-        public static final int firstLowDepth=0x7f050005;
-        public static final int firstLowTime=0x7f050004;
-        public static final int label=0x7f050003;
-        public static final int place=0x7f050000;
-        public static final int secondHigh=0x7f05000c;
-        public static final int secondHighDepth=0x7f05000e;
-        public static final int secondHighTime=0x7f05000d;
-        public static final int secondLow=0x7f050009;
-        public static final int secondLowDepth=0x7f05000b;
-        public static final int secondLowTime=0x7f05000a;
-        public static final int tableLayout1=0x7f050001;
+        public static final int firstHigh=0x7f070006;
+        public static final int firstHighDepth=0x7f070008;
+        public static final int firstHighTime=0x7f070007;
+        public static final int firstLow=0x7f070002;
+        public static final int firstLowDepth=0x7f070005;
+        public static final int firstLowTime=0x7f070004;
+        public static final int label=0x7f070003;
+        public static final int place=0x7f070000;
+        public static final int secondHigh=0x7f07000c;
+        public static final int secondHighDepth=0x7f07000e;
+        public static final int secondHighTime=0x7f07000d;
+        public static final int secondLow=0x7f070009;
+        public static final int secondLowDepth=0x7f07000b;
+        public static final int secondLowTime=0x7f07000a;
+        public static final int tableLayout1=0x7f070001;
+    }
+    public static final class integer {
+        public static final int image_ping_interval=0x7f050000;
+        public static final int request_timeout=0x7f050001;
+        public static final int socket_buffer_size=0x7f050002;
+        public static final int vibro_response_duration=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int change_coords_url_format_title=0x7f040003;
+        public static final int change_image_url_title=0x7f040002;
+        public static final int click_processed_successfully_mark=0x7f04000d;
+        public static final int coordinates_acceptor_url_format=0x7f040009;
+        public static final int coordinates_url_format_preference_name=0x7f04000c;
         public static final int hello=0x7f040000;
+        public static final int image_cache_file_name=0x7f04000b;
+        public static final int image_provider_url=0x7f040008;
+        public static final int menu_change_coords_url_format=0x7f040007;
+        public static final int menu_change_image_url=0x7f040006;
+        public static final int menu_start_pinging=0x7f040004;
+        public static final int menu_stop_pinging=0x7f040005;
+        public static final int url_preference_name=0x7f04000a;
     }
 }
